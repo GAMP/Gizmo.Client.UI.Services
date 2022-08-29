d1d4b1e692c96727be77b8490af5314a08aa646f
klapsis-netprojects <klapsis@netprojects.gr>
2022-08-29 16:42:42 +0300

    Update client services.

---
+++ Gizmo.Client.UI.Services/View/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using Gizmo.Client.UI.View.States;
+using Gizmo.UI.View.Services;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Gizmo.Client.UI.View.Services
+{
+    [Register()]
+    public sealed class UserService : ViewStateServiceBase<UserViewState>, IDisposable
+    {
+        #region CONSTRUCTOR
+        public UserService(UserViewState viewState,
+            ILogger<UserService> logger,
+            IServiceProvider serviceProvider) : base(viewState, logger, serviceProvider)
+        {
+            viewState.Username = "Infidel 06";
+            viewState.RegistrationDate = new DateTime(2020, 3, 4);
+            viewState.Balance = 10.76m;
+            viewState.CurrentTimeProduct = "Six Hours (6) for 10$ Pack";
+            viewState.Time = new TimeSpan(6, 36, 59);
+            viewState.Points = 416;
+            viewState.Picture = "_content/Gizmo.Client.UI/img/Cyber Punks.png";
+        }
+        #endregion
+
+        public Task LockAsync()
+        {
+            ViewState.IsLocked = true;
+            ViewState.RaiseChanged();
+            return Task.CompletedTask;
+        }
+
+        public Task UnlockAsync()
+        {
+            ViewState.IsLocked = false;
+            ViewState.RaiseChanged();
+            return Task.CompletedTask;
+        }
+
+        public Task LogοutAsync()
+        {
+            NavigationService.NavigateTo("/");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Gizmo.Client.UI.Services/View/States/UserViewState.cs
@@ -0,0 +1,79 @@
+﻿using Gizmo.UI.View.States;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gizmo.Client.UI.View.States
+{
+    [Register()]
+    public sealed class UserViewState : ViewStateBase
+    {
+        #region FIELDS
+        private int _id;
+        private string _username = String.Empty;
+        private DateTime _registrationDate;
+        private decimal _balance;
+        private string _currentTimeProduct = String.Empty;
+        private TimeSpan _time;
+        private int _points;
+        private string _picture = String.Empty;
+        private bool _isLocked;
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            internal set { SetProperty(ref _id, value); }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            internal set { SetProperty(ref _username, value); }
+        }
+
+        public DateTime RegistrationDate
+        {
+            get { return _registrationDate; }
+            internal set { SetProperty(ref _registrationDate, value); }
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            internal set { SetProperty(ref _balance, value); }
+        }
+
+        public string CurrentTimeProduct
+        {
+            get { return _currentTimeProduct; }
+            internal set { SetProperty(ref _currentTimeProduct, value); }
+        }
+
+        public TimeSpan Time
+        {
+            get { return _time; }
+            internal set { SetProperty(ref _time, value); }
+        }
+
+        public int Points
+        {
+            get { return _points; }
+            internal set { SetProperty(ref _points, value); }
+        }
+
+        public string Picture
+        {
+            get { return _picture; }
+            internal set { SetProperty(ref _picture, value); }
+        }
+
+        public bool IsLocked
+        {
+            get { return _isLocked; }
+            internal set { SetProperty(ref _isLocked, value); }
+        }
+
+        #endregion
+    }
+}
