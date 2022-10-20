3b38ef8002ed4a73adf77b80c29536a349592583
klapsis-netprojects <klapsis@netprojects.gr>
2022-10-20 17:22:56 +0300

    Update UserSettingsViewState.cs

--- Gizmo.Client.UI.Services/View/States/UserSettingsViewState.cs
+++ Gizmo.Client.UI.Services/View/States/UserSettingsViewState.cs
@@ -10,7 +10,15 @@ namespace Gizmo.Client.UI.View.States
     {
         #region FIELDS
         private string _username = string.Empty;
-        private string _homePhone = string.Empty;
+        private string _firstName = string.Empty;
+        private string _lastName = string.Empty;
+        private DateTime? _birthDate;
+        private Sex _sex = Sex.Unspecified;
+        private string _country = string.Empty;
+        private string _address = string.Empty;
+        private string _email = string.Empty;
+        private string _phone = string.Empty;
+        private string _mobilePhone = string.Empty;
         #endregion
 
         #region PROPERTIES
@@ -26,14 +34,67 @@ namespace Gizmo.Client.UI.View.States
             set { SetProperty(ref _username, value); }
         }
 
-        /// <summary>
-        /// Gets or sets home phone.
-        /// </summary>
         [ValidatingProperty()]
-        public string HomePhone
+        public string FirstName
         {
-            get { return _homePhone; }
-            set { SetProperty(ref _homePhone, value); }
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        [ValidatingProperty()]
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+
+        [ValidatingProperty()]
+        public DateTime? BirthDate
+        {
+            get { return _birthDate; }
+            set { SetProperty(ref _birthDate, value); }
+        }
+
+        [ValidatingProperty()]
+        public Sex Sex
+        {
+            get { return _sex; }
+            set { SetProperty(ref _sex, value); }
+        }
+
+        [ValidatingProperty()]
+        public string Country
+        {
+            get { return _country; }
+            set { SetProperty(ref _country, value); }
+        }
+
+        [ValidatingProperty()]
+        public string Address
+        {
+            get { return _address; }
+            set { SetProperty(ref _address, value); }
+        }
+
+        [ValidatingProperty()]
+        public string Email
+        {
+            get { return _email; }
+            set { SetProperty(ref _email, value); }
+        }
+
+        [ValidatingProperty()]
+        public string Phone
+        {
+            get { return _phone; }
+            set { SetProperty(ref _phone, value); }
+        }
+
+        [ValidatingProperty()]
+        public string MobilePhone
+        {
+            get { return _mobilePhone; }
+            set { SetProperty(ref _mobilePhone, value); }
         }
 
         #endregion
