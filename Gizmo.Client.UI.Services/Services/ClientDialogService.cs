a77415f51db2c8d34bb8ebd30482ac1c690d1aa4
Oleg Savelos <olegsavelos@gmail.com>
2022-11-16 15:25:23 +0200

    DialogService base updates.

--- Gizmo.Client.UI.Services/Services/ClientDialogService.cs
+++ Gizmo.Client.UI.Services/Services/ClientDialogService.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Gizmo.Client.UI.Services.Services
 {
-    public sealed class ClientDialogService : DialogService, IClientDialogService
+    public sealed class ClientDialogService : DialogServiceBase, IClientDialogService
     {
         public ClientDialogService(IServiceProvider serviceProvider, ILogger<ClientDialogService> logger) : base(serviceProvider, logger)
         {
