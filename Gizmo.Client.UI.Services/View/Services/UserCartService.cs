16a97de279d2e2127a6b93a2c5204ec81bef54c8
klapsis-netprojects <klapsis@netprojects.gr>
2023-01-12 11:26:26 +0200

    Update UserCartService.cs

--- Gizmo.Client.UI.Services/View/Services/UserCartService.cs
+++ Gizmo.Client.UI.Services/View/Services/UserCartService.cs
@@ -75,11 +75,11 @@ namespace Gizmo.Client.UI.View.Services
 
             ViewState.RaiseChanged();
 
-            //TODO: A GET CURRENT URI IF NOT SHOP OR PRODUCT DETAILS THEN NAVIGATE TO SHOP.
-
+            //If current uri is not shop or product details then navigate to shop.
             var currentUri = NavigationService.GetUri();
 
-            NavigationService.NavigateTo(ClientRoutes.ShopRoute);
+            if (!currentUri.EndsWith("/shop") && !currentUri.Contains("/productdetails/"))
+                NavigationService.NavigateTo(ClientRoutes.ShopRoute);
         }
 
         public Task RemoveProductAsync(int productId, int? quantity)
