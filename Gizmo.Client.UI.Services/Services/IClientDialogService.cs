613b166a7198793556c2f04ada67010c25c51547
klapsis-netprojects <klapsis@netprojects.gr>
2023-04-13 11:12:27 +0300

    Update user profile.

--- Gizmo.Client.UI.Services/Services/IClientDialogService.cs
+++ Gizmo.Client.UI.Services/Services/IClientDialogService.cs
@@ -1,5 +1,4 @@
-﻿using System.Xml.Linq;
-using Gizmo.Client.UI.View.States;
+﻿using Gizmo.Client.UI.View.States;
 using Gizmo.UI.Services;
 
 namespace Gizmo.Client.UI.Services
@@ -9,6 +8,7 @@ namespace Gizmo.Client.UI.Services
         Task<ShowDialogResult<EmptyDialogResult>> ShowCheckoutDialogAsync(CancellationToken cancellationToken = default);
         Task<ShowDialogResult<EmptyDialogResult>> ShowExecutableSelectorDialogAsync(int applicationId, CancellationToken cancellationToken = default);
         Task<ShowDialogResult<UserAgreementResult>> ShowUserAgreementDialogAsync(UserAgreementDialogParameters userAgreementDialogParameters, CancellationToken cancellationToken = default);
+        Task<ShowDialogResult<EmptyDialogResult>> ShowChangeProfileDialogAsync(CancellationToken cancellationToken = default);
         Task<ShowDialogResult<EmptyDialogResult>> ShowChangeEmailDialogAsync(CancellationToken cancellationToken = default);
         Task<ShowDialogResult<EmptyDialogResult>> ShowChangeMobileDialogAsync(CancellationToken cancellationToken = default);
         Task<ShowDialogResult<EmptyDialogResult>> ShowChangePasswordDialogAsync(CancellationToken cancellationToken = default);
