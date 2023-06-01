6c1cf3e6d3bb000bdf2dc47e9128089be3117503
Oleg Savelos <olegsavelos@gmail.com>
2023-06-01 11:24:20 +0300

    Notifications host support.

---
+++ Gizmo.Client.UI.Services/Services/WebNotificationHost.cs
@@ -0,0 +1,17 @@
+﻿using Gizmo.UI;
+
+namespace Gizmo.Client.UI.Services
+{
+    public sealed class WebNotificationHost : INotificationsHost
+    {
+        public Task HideAsyc()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task ShowAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
