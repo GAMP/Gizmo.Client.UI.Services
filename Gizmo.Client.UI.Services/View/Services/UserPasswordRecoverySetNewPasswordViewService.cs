3b82f2a508c6df030e9dccfd867f68eccdafbc2d
klapsis-netprojects <klapsis@netprojects.gr>
2023-06-29 10:17:43 +0300

    Updates.

--- Gizmo.Client.UI.Services/View/Services/UserPasswordRecoverySetNewPasswordViewService.cs
+++ Gizmo.Client.UI.Services/View/Services/UserPasswordRecoverySetNewPasswordViewService.cs
@@ -141,8 +141,8 @@ namespace Gizmo.Client.UI.View.Services
                 }
             }
 
-            Regex lowerRulePassedRegex = new Regex("[A-Z]");
-            Regex upperRuleRegex = new Regex("[a-z]");
+            Regex lowerRulePassedRegex = new Regex("[a-z]");
+            Regex upperRuleRegex = new Regex("[A-Z]");
             Regex numberRuleRegex = new Regex("[0-9]");
 
             if (ViewState.PasswordTooltip.HasLowerCaseCharactersRule)
