0f948d9efbf19415c728776c7252840be24f344f
klapsis-netprojects <klapsis@netprojects.gr>
2023-09-06 16:27:42 +0300

    Updates.

---
+++ Gizmo.Client.UI.Services/View/Services/UserBalanceTooltipViewService.cs
@@ -0,0 +1,38 @@
+﻿using Gizmo.Client.UI.View.States;
+using Gizmo.UI.View.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Client.UI.View.Services
+{
+    /// <summary>
+    /// User balance view state service.
+    /// </summary>
+    /// <remarks>
+    /// Responsible of updating user balance view state.
+    /// </remarks>
+    [Register()]
+    public sealed class UserBalanceTooltipViewService : ViewStateServiceBase<UserBalanceTooltipViewState>
+    {
+        public UserBalanceTooltipViewService(UserBalanceTooltipViewState viewState,
+            ILogger<UserBalanceViewService> logger,
+            IServiceProvider serviceProvider,
+            IOptions<ClientShopOptions> shopOptions,
+            IOptions<UserOnlineDepositOptions> userOnlineDepositOptions) : base(viewState, logger, serviceProvider)
+        {
+            _shopOptions = shopOptions;
+            _userOnlineDepositOptions = userOnlineDepositOptions;
+        }
+
+        private readonly IOptions<ClientShopOptions> _shopOptions;
+        private readonly IOptions<UserOnlineDepositOptions> _userOnlineDepositOptions;
+
+        protected override Task OnInitializing(CancellationToken ct)
+        {
+            ViewState.DisableShop = _shopOptions.Value.Disabled;
+            ViewState.DisableOnlineDeposits = _userOnlineDepositOptions.Value.Disabled;
+            return base.OnInitializing(ct);
+        }
+    }
+}
---
+++ Gizmo.Client.UI.Services/View/States/UserBalanceTooltipViewState.cs
@@ -0,0 +1,17 @@
+﻿using Gizmo.UI.View.States;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gizmo.Client.UI.View.States
+{
+    [Register()]
+    public sealed class UserBalanceTooltipViewState : ViewStateBase
+    {
+        #region PROPERTIES
+
+        public bool DisableShop { get; internal set; }
+
+        public bool DisableOnlineDeposits { get; internal set; }
+
+        #endregion
+    }
+}
